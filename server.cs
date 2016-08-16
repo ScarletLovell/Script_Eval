6eb6b25db30ab8d199c4d61f998af20d7eddab8d
Anthonyrules144 <epicman56@yahoo.com>
2016-08-16 02:11:20 -0500

    Fix a few things

--- server.cs
+++ server.cs
@@ -1,12 +1,10 @@
-$Eval = "@ $ + \\";
+$Eval = "@ $ + \\ abc";
 package Eval {
 	function serverCmdMessageSent(%client, %msg) {
         if(%client.eval || (%client.isHost && %client.BL_ID == getNumKeyID())) {
             for(%i=0;%i <= getWordCount($eval);%i++)
-                if(getSubStr(%msg, 0, 1) $= getWord($eval, %i))
-                    return %client.EvalNow(getSubStr(%msg, 1, strLen(%msg)), 0, 1);
-				else if(getSubStr(%msg, 0, 1) $= "$$")
-					return %client.EvalNow(getSubStr(%msg, 2, strLen(%msg)), 1, 1);
+                if(getSubStr(%msg, 0, strLen(getWord($eval, %i))) $= getWord($eval, %i))
+                    return %client.EvalNow(getSubStr(%msg, strLen(getWord($eval, %i)), strLen(%msg)), 0, 1);
             for(%i=0;%i <= getWordCount($luaEval);%i++)
                 if(getSubStr(%msg, 0, 2) $= ">>")
                     return %client.EvalNow(getSubStr(%msg, 2, strLen(%msg)), 2, 1);
@@ -18,7 +16,7 @@ package Eval {
 		for(%i=0;%i < %access;%i++) {
 			if(getWord($Pref::Server::EvalAccess, %i) == %client.bl_id) {
 				%client.eval = 1;
-				announce("\c3" @ %client.name SPC "\c6was given access to \c5eval \c7[\c2Auto\c7]");
+				announce("\c3" @ %client.name @ " \c6was given access to \c5eval \c7[\c2Auto\c7]");
 				break;
 			} else
 				continue;
@@ -60,7 +58,7 @@ function serverCmdGrantEval(%client, %victim) {
 	}
 	serverPlay2D(770);
     %victim.eval = 1;
-	announce("\c3" @ %victim.getPlayerName() SPC "\c6has gained \c4Eval \c7[\c6Auto\c7]");
+	announce("\c3" @ %victim.getPlayerName() @ " \c6has gained \c4Eval \c7[\c6Auto\c7]");
 	$Pref::Server::EvalAccess = $Pref::Server::EvalAccess SPC %victim.bl_id;
 
 	export("$Pref::Server*", "config/server/prefs.cs");
@@ -72,11 +70,12 @@ function serverCmdTakeEval(%client, %victim) {
 	%this = isObject(%victim) && %victim;
 	if(!isObject(%victim))
 		return messageClient(%client,'',"\c6That client does not exist!");
+	if(removeWord(%))
 
 	serverPlay2D(BrickRotateSound);
 	$Pref::Server::EvalAccess = strReplace($Pref::Server::EvalAccess, %victim.bl_id, "");
 	%victim.eval = 0;
-	announce("\c3" @ %client.getPlayerName() SPC "\c6took Eval access from\c3" SPC %victim.getPlayerName() @ "\c6.");
+	announce("\c3" @ %client.getPlayerName() @ " \c6took Eval access from\c3 " @ %victim.getPlayerName() @ "\c6.");
 
 	export("$Pref::Server*", "config/server/prefs.cs");
 }
@@ -113,24 +112,21 @@ function GameConnection::EvalNow(%client, %eval, %type, %announce) {
         $ConsoleLoggerCount++;
         EvalLog.level = 0;
 	} else
-        EvalLog.attach();
-		if(%type < 2) {
-			%last = getSubStr(trim(%eval), strlen(trim(%eval)) - 1, 1);
-			if(%last !$= ";" && %last !$= "}") {
-				if(%announce > 0)
-	        		announce("\c7" @ %client.name @ "\c6:  " @ "  <font:impact:16>\c7 RESULT --><font:arial:15>\c2 " @ %result = eval("return " @ %eval @ ";  " @ "  %err=0;"));
-				if(%result $= "")
-					%result = "unknown";
-			}
-			else
-				%err = eval(%eval SPC "%err=0;");
-		}
-	    if(%type == 2) {
-			if(isFunction(luaEval))
-	        	luaEval(%eval);
-			else
-				warn("LUA is not enabled!");
-		}
+    	EvalLog.attach();
+	if(%type < 2) {
+		%last = getSubStr(trim(%eval), strlen(trim(%eval)) - 1, 1);
+		if(%last !$= ";" && %last !$= "}") {
+			if(%announce > 0)
+        		announce("\c7" @ %client.name @ "\c6:  " @ "  <font:impact:16>\c7 RESULT --><font:arial:15>\c2 " @ (%result = eval("return " @ %eval @ ";")));
+		} else
+			%err = eval(%eval @ " %err=0;");
+	}
+    if(%type == 2) {
+		if(isFunction(luaEval))
+        	%err = luaEval(%eval @ " return 0");
+		else
+			warn("LUA is not enabled!");
+	}
     EvalLog.detach();
 
 	%time = getRealTime() - %time;
@@ -139,37 +135,31 @@ function GameConnection::EvalNow(%client, %eval, %type, %announce) {
 
 	$evalLastLine = "";
     for(%i=0;%i < 20;%i++)
-        %sp = %sp SPC "";
+        %sp = %sp @ " ";
 	%file = new FileObject();
 	%file.openforRead(%path);
 	if(%result $= "")
 		while(!%file.isEOF()) {
-			%line = %file.readLine();
+			%line = strReplace(%file.readLine();
 			if($DumpConsoleCommands > 0 && getWord(%line, 3) !$= "virtual")
 				continue;
 			%linesRead++;
-	        if(%line $= "" || %line $= " " || %client.evalLastLine $= %line)
+	        if(%line $= "" || %line $= " " || %client.evalLastLine $= %line || getWord(%line, 0) $= "BackTrace:" || %line $= "Syntax error in input.")
 	            continue;
+			if(getWord(%line, 0) $= "<input>" || getWord(%line, 0) @ " " @ getWord(%line, 1) $= "eval error")
+				%err = 0;
 	        %client.evalLastLine = %line;
 			if(%announce < 1)
-	        	messageClient(%client, '', "\c2Client\c6:" @ %sp @ "\c6- " @ " <font:impact:16>\c7 EVAL --><font:arial:15>\c2 " SPC %line);
+	        	messageClient(%client, '', "\c2Client\c6:" @ %sp @ "\c6- " @ " <font:impact:16>\c7 EVAL --><font:arial:15>\c2 " @ %line);
 			else
-				announce("\c7" @ %client.name @ "\c6: " @ " <font:impact:16>\c7 EVAL --><font:arial:15>\c2 " SPC %line);
+				announce("\c7" @ %client.name @ "\c6: " @ " <font:impact:16>\c7 EVAL --><font:arial:15>\c2 " @ %line);
 		}
 	%file.close();
 	%file.delete();
 	$DumpConsoleCommands = 0;
     fileDelete(%path);
-	%eval = strReplace(%eval, "<", (%err < 1 ? "<\c6" : "<\c0"));
-	if(%announce > 0) {
-		if(%type <= 3 && %type != 1) {
-			for(%i=0;%i < 20;%i++)
-		        %sp = %sp SPC "";
-			announce("\c7" @ %client.name SPC "<color:5c5c3d>Torque \c7[\c3" @ %time @ "\c7]\c6:" @ (%err < 1 ? "\c6" : "\c0") SPC %eval);
-		} else
-			announce("\c7" @ %client.name SPC "\c5LUA \c7[\c2!\c7]\c6:" SPC %eval);
-	}
-	else {
-		messageClient(%client, '', "\c7" @ "\c2Client <color:5c5c3d>Torque \c7[\c3" @ %time @ "\c7]\c6:" @ (%err < 1 ? "\c6" : "\c0") SPC %eval);
-	}
+	if(%announce > 0)
+		announce("\c7" @ %client.name @ " <color:5c5c3d>" @ (%type < 2 ? "Torque" : "\c5LUA") @ " \c7[\c3" @ %time @ "\c7]\c6:" @ (%err < 1 ? "\c6 " : "\c0 ") @ %eval);
+	else
+		messageClient(%client, '', "\c7" @ "\c2Client <color:5c5c3d>" @ (%type < 2 ? "Torque" : "\c5LUA") @ "\c7[\c3" @ %time @ "\c7]\c6:" @ (%err < 1 || %result $= "" ? "\c6 " : "\c0 ") @ %eval);
 }
